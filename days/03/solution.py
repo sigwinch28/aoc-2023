bdd020c4333073b9a3b1c1ddf3624e881961c309
Joe Harrison <joe@sigwinch.uk>
2023-12-03 12:38:39 +0000

    day 03 part 2

--- days/03/solution.py
+++ days/03/solution.py
@@ -29,9 +29,9 @@ SchematicObject = Union[Number, Symbol]
 
 
 def parse_input(text: str) -> Iterator[SchematicObject]:
-    for y, line in enumerate(text.splitlines()):
+    for y, line in enumerate(text.splitlines(), 1):
         currentNumber = None
-        for x, char in enumerate(line):
+        for x, char in enumerate(line, 1):
             if char.isdigit():
                 if currentNumber is None:
                     currentNumber = Number(
@@ -56,20 +56,21 @@ def parse_input(text: str) -> Iterator[SchematicObject]:
             currentNumber = None
 
 
+def number_adjacent_coords(number: Number) -> Iterator[tuple[int, int]]:
+    for x in range(number["xSpan"][0] - 1, number["xSpan"][1] + 2):
+        for y in range(number["y"] - 1, number["y"] + 2):
+            if y == number["y"] and (
+                x >= number["xSpan"][0] and x <= number["xSpan"][1]
+            ):
+                continue
+            yield (x, y)
+
+
 def part1(raw_input: str):
     objs: list[SchematicObject] = list(parse_input(raw_input))
     symbols = [x for x in objs if x["kind"] == "symbol"]
     numbers = [x for x in objs if x["kind"] == "number"]
     symbol_positions = set(((symbol["x"], symbol["y"]) for symbol in symbols))
-
-    def number_adjacent_coords(number: Number) -> Iterator[tuple[int, int]]:
-        for x in range(number["xSpan"][0] - 1, number["xSpan"][1] + 2):
-            for y in range(number["y"] - 1, number["y"] + 2):
-                if y == number["y"] and (
-                    x >= number["xSpan"][0] and x <= number["xSpan"][1]
-                ):
-                    continue
-                yield (x, y)
 
     def number_is_adjacent_any_symbol(number: Number):
         for coord in number_adjacent_coords(number):
@@ -86,10 +87,28 @@ def part1(raw_input: str):
 
 
 def part2(raw_input: str):
-    pass
+    objs: list[SchematicObject] = list(parse_input(raw_input))
+    symbols = [x for x in objs if x["kind"] == "symbol"]
+    numbers = [x for x in objs if x["kind"] == "number"]
+
+    gear_candidates: dict[tuple[int, int], list[Number]] = {
+        (symbol["x"], symbol["y"]): [] for symbol in symbols if symbol["value"] == "*"
+    }
+
+    for number in numbers:
+        for coord in number_adjacent_coords(number):
+            if coord in gear_candidates:
+                gear_candidates[coord].append(number)
+
+    gear_ratios = [
+        reduce(lambda x, y: x * y, map(lambda x: x["value"], v))
+        for v in gear_candidates.values()
+        if len(v) == 2
+    ]
+    return sum(gear_ratios)
 
 
 if __name__ == "__main__":
-    print(part1(SAMPLE_PATHS[0].read_text()))
+    # print(part1(SAMPLE_PATHS[0].read_text()))
     print(part1(INPUT_PATH.read_text()))
-    # print(part2(INPUT_PATH.read_text()))
+    print(part2(INPUT_PATH.read_text()))
