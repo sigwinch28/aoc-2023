4fecaff8690e8e5239839e60c2845ee8aa8b94e4
Joe Harrison <joe.harrison@brevanhoward.com>
2023-12-04 13:12:54 +0000

    day 04 part 1

---
+++ days/04/solution.py
@@ -0,0 +1,55 @@
+from functools import reduce
+from pathlib import Path
+
+from typing import Iterator, Literal, TypedDict, Union
+
+
+INPUT_PATH = Path(__file__).parent / "input.txt"
+SAMPLE_PATHS = [
+    Path(__file__).parent / "sample1.txt",
+]
+
+
+class Card(TypedDict):
+    number: int
+    left_side: set[int]
+    right_side: set[int]
+
+
+def parse_input(text: str) -> Iterator[Card]:
+    for line in text.splitlines():
+        [meta, contents] = line.split(":", maxsplit=1)
+        card_number = int(meta.rsplit()[1])
+        [lhs, rhs] = contents.split("|", maxsplit=1)
+        left_side = set(map(int, lhs.strip().split()))
+        right_side = set(map(int, rhs.strip().split()))
+
+        yield Card(number=card_number, left_side=left_side, right_side=right_side)
+
+
+def part1(raw_input: str):
+    cards = parse_input(raw_input)
+
+    def result(card: Card):
+        num_matches = len(card["left_side"] & card["right_side"])
+        score = 0
+        if num_matches > 0:
+            score = pow(2, num_matches - 1)
+
+        return score
+
+    map(result, cards)
+
+    return sum(map(result, cards))
+
+
+def part2(raw_input: str):
+    input = parse_input(raw_input)
+
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    # print(part1(SAMPLE_PATHS[0].read_text()))
+    print(part1(INPUT_PATH.read_text()))
+    # print(part2(INPUT_PATH.read_text()))
