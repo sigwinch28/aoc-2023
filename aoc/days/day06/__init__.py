3115b58353167d7749304f28e5117b17d752a46c
Joe Harrison <joe@sigwinch.uk>
2023-12-06 09:32:54 +0000

    day 06 part 2

--- aoc/days/day06/__init__.py
+++ aoc/days/day06/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import reduce
 from typing import Iterable
 
 
@@ -8,29 +9,33 @@ class Race:
     record: int
 
 
-def parse_input(text: str) -> Iterable[Race]:
-    def parse_line(line: str) -> Iterable[int]:
-        return map(int, line.split(":", maxsplit=1)[1].strip().split())
-
-    return map(
-        lambda t, d: Race(time=t, record=d), *(map(parse_line, text.splitlines()))
-    )
-
-
-def part1(raw_input: str):
-    races = parse_input(raw_input)
-    ways = 1
-    for race in races:
-        ways_to_win_this_race = 0
-        for t_hold in range(0, race.time + 1):
-            distance = t_hold * (race.time - t_hold)
-            if distance > race.record:
-                ways_to_win_this_race += 1
-        ways *= ways_to_win_this_race
-
+def ways_to_win(race: Race) -> int:
+    ways = 0
+    for t_hold in range(0, race.time + 1):
+        distance = t_hold * (race.time - t_hold)
+        if distance > race.record:
+            ways += 1
     return ways
 
 
+def part1(raw_input: str):
+    def parse_line(line: str) -> Iterable[int]:
+        return map(int, line.split(":", maxsplit=1)[1].strip().split())
+
+    races = list(
+        map(
+            lambda t, d: Race(time=t, record=d),
+            *(map(parse_line, raw_input.splitlines()))
+        )
+    )
+
+    return reduce(lambda x, y: x * y, map(ways_to_win, races))
+
+
 def part2(raw_input: str):
-    input = parse_input(raw_input)
-    return 0
+    def parse_line(line: str) -> int:
+        return int("".join(line.split(":", maxsplit=1)[1].strip().split()))
+
+    [t, r] = list(map(parse_line, raw_input.splitlines()))
+    race = Race(time=t, record=r)
+    return ways_to_win(race)
