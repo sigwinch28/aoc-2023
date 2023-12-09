402deebcfe8896ae5f3910dfb25a3627c3e442c8
Joe Harrison <joe@sigwinch.uk>
2023-12-09 11:38:07 +0000

    simplify

--- aoc/days/day09/__init__.py
+++ aoc/days/day09/__init__.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterable, Literal
+from typing import Iterable
 
 
 def parse_input(text: str) -> list[list[int]]:
@@ -22,22 +22,20 @@ def all_differences(ints: Iterable[int]) -> Iterable[list[int]]:
     yield current
 
 
-def extrapolate(
-    ints: list[int], idx: Literal[-1, 0], op: Callable[[int, int], int]
-) -> int:
+def extrapolate(ints: list[int]) -> int:
     difference = list(reversed(list(all_differences(ints))))
     value = 0
     for i in range(0, len(difference)):
-        value = op(difference[i][idx], value)
+        value = difference[i][-1] + value
 
     return value
 
 
 def part1(raw_input: str):
     input = parse_input(raw_input)
-    return sum(extrapolate(i, idx=-1, op=lambda x, y: x + y) for i in input)
+    return sum(extrapolate(i) for i in input)
 
 
 def part2(raw_input: str):
     input = parse_input(raw_input)
-    return sum(extrapolate(i, idx=0, op=lambda x, y: x - y) for i in input)
+    return sum(extrapolate(list(reversed(i))) for i in input)
